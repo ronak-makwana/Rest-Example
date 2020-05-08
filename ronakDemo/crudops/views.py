31405c500128d50ddc7038ffa73d88d47bc1f64e
ronak_makwana <ronak.makwana@peerbits.com>
2020-05-08 12:05:32 +0530

    remove a directory

--- ronakDemo/crudops/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from . import serializer
-from .models import DemoModel
-# Create your views here.
-class modelviewset(viewsets.ModelViewSet):
-    queryset = DemoModel.objects.all()
-    serializer_class= serializer.modelserializer
--- ronakDemo/ronakDemo/router.py
+++
@@ -1,5 +0,0 @@
-from crudops.views import modelviewset
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register('data',modelviewset)
